2a7eebd80aa593cea66cdb7fa48a7aa528165f78
Sarah <mccries@sheridancollege.ca>
2023-02-01 11:57:43 -0500

    I'm Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -7,12 +10,14 @@ package exercise1;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022 
+ * @author Sarah McCrie 991405606 - Jan 26, 2023
  */
 public class CardTrick {
     
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random rand = new Random();
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
@@ -21,43 +26,83 @@ public class CardTrick {
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
+            card.setValue(rand.nextInt(12));
+            card.setSuit(Card.SUITS[rand.nextInt(3)]);
+            hand[i]=card;
         }
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a card number between 1 and 13");
+        int value = scan.nextInt();
+        System.out.println("Please enter a number to represent the suit: Enter 1 for Hearts, 2 for Diamonds, 3 for Spades, or 4 for Clubs.");
+        int suitAsInt = scan.nextInt();
+        scan.close();
+
+        String suit = "";
+        switch(suitAsInt){
+            case 1:
+                suit = hand[0].getSuit();
+            case 2: 
+                suit = hand[1].getSuit();
+                break;
+            case 3:
+                suit = hand[2].getSuit();
+                break;
+            case 4:
+                suit = hand[3].getSuit();
+                break;
+            }
+      
         // Hint: You can ask for values 1 to 10, and then
         //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
         //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
         // 
-        // Then loop through the cards in the array to see if there's a match.
         
+        // Then loop through the cards in the array to see if there's a match.     
         // If the guess is successful, print System.out.println("Congratulations, you guessed right!");.
         
+            for(int i = 0; i < hand.length; i++){
+                if(suit == hand[i].getSuit() && value == hand[i].getValue()){
+                    printInfo();
+                    return;
+                }
+            }
+            for(int i = 0; i < hand.length; i++){
+                if(suit != hand[i].getSuit() || value != hand[i].getValue()){
+                   System.out.println("Sorry, you guessed incorrectly!");
+                   return;
+                }
+            } 
     }
+        
+        // If the guess is successful, invoke the printInfo() method below.
 
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
+     * @author Sarah McCrie Jan 2023
      */
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Sarah");
         System.out.println();
         
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+        System.out.println("My ambitions:");
+        System.out.println("-- start my career");
+        System.out.println("-- get a bunch of dogs");
+	    System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- painting");
+        System.out.println("-- sculpting");
+        System.out.println("-- walking");
+        System.out.println("-- renovating");
 
         System.out.println();
         
